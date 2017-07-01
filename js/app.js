0206efa22e85208529dc6627cf92dc5452bf29d0
fmls1989 <fmls1989@gmail.com>
2017-07-01 19:37:43 +0100

    game added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('hola');
+window.addEventListener('DOMContentLoaded', () => {
+  var score = 0,
+      mySquare;
+  const lis = document.getElementsByTagName('li');
+
+  setInterval(function(){
+    mySquare = lis[Math.floor(Math.random()*lis.length)];
+    mySquare.className = 'coloredSquare';
+    mySquare.addEventListener('click', getScore);
+
+    function getScore(){
+      score+=10;
+      console.log(score);
+    }
+
+    setTimeout(function(){
+      mySquare.removeEventListener('click', getScore);
+      mySquare.classList.remove('coloredSquare');
+    }, 1000);
+  }, 1100);
+});
